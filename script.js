f4588ed444ab42dba7cb2e30384bb3a91d999ef1
MatheusMathias1914 <matheus7mathias7@gmail.com>
2024-11-27 13:39:00 -0300

    ATUALIZAÇÃO DE PATH, FONTES E H1

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário para testes
 
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); //
+  
     const nome = event.target.nome.value.trim();
     const email = event.target.email.value.trim();
     const mensagem = event.target.mensagem.value.trim();
-
-    // Verificar se o nome, email e mensagem não estão vazios
+  
+   
     if (!nome || !email || !mensagem) {
-        alert('Por favor, preencha todos os campos.');
-        return;
+      alert("Por favor, preencha todos os campos.");
+      return;
     }
-
-    // Validação de email (verificação simples)
+  
+    // Verificar se o email é válido
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailRegex.test(email)) {
-        alert('Por favor, insira um email válido.');
-        return;
+      alert("Por favor, insira um email válido.");
+      return;
     }
-
-    // Exibir mensagem de sucesso
-    alert('Mensagem enviada com sucesso!');
-
-    // Resetar o formulário
+  
+    
+    alert("Mensagem enviada com sucesso!");
+    
+    // Limpa o formulário após o envio
     event.target.reset();
-});
+  });
+  
+  // Formatação do valor da doação
+  const valorInput = document.getElementById("valor");
+  
+  valorInput.addEventListener("input", function () {
+    let value = valorInput.value.replace(/\D/g, ""); // Remove qualquer coisa que não seja número
+    
+    if (value.length > 2) {
+      value = value.replace(/(\d)(\d{2})$/, "$1,$2");
+    }
+  
+    
+    valorInput.value = value ? "R$ " + value.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.") : "";
+  });
+  
